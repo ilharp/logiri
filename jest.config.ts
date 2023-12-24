71b4c1b9f7249ac47cdd76ef36b3fe4cae48ffc0
Il Harper <hi@ilharper.com>
2023-12-24 18:04:55 +0800

    test: init test

---
+++ jest.config.ts
@@ -0,0 +1,19 @@
+import type { JestConfigWithTsJest } from 'ts-jest'
+
+const config: JestConfigWithTsJest = {
+  preset: 'ts-jest/presets/default-esm',
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+}
+
+// eslint-disable-next-line import/no-default-export
+export default config
